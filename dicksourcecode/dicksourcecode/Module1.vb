81d9da9fe63a1f1d34d63fdb8de024f62f10033d
Tenvy <87992922+Tenvy@users.noreply.github.com>
2022-01-10 10:14:56 +0800

    Add files via upload

---
+++ dicksourcecode/dicksourcecode/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim owo = 0
+        Dim uwu = 1
+
+        Do
+            owo += 1
+            Console.Write(1001010001 & "     1001   1000110   " & "1001010   00100110")
+            Threading.Thread.Sleep(100)
+        Loop
+
+
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
